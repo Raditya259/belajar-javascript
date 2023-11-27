b364c688c814d468d0eef10be64d82edd982f8b0
Raditya Lungguk Satya Putra <radityalungguk@gmail.com>
2023-11-27 21:05:09 +0700

    -

--- 6-b-tugas-looping-lanjutan.js
+++ 6-b-tugas-looping-lanjutan.js
@@ -4,19 +4,24 @@
 // Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
 // menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
 //tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+console.log("Soal Nomor 1. Melakukan Looping Menggunakan While");
 console.log("Looping Pertama atau Maju");
 let a = 1
+
 while (a <= 5) {
     console.log(a);
     a++;
 }
 
-let b = 5
 console.log("Looping Kedua atau Mundur");
+let b = 5
+
 while (b >= 1) {
     console.log(b);
     b--;
 }
+console.log();
 
 // 2. Melakukan Looping Menggunakan For
 // Problem
@@ -24,6 +29,18 @@ while (b >= 1) {
 // Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
 // menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
 //tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+console.log("Soal Nomor 2. Melakukan Looping Menggunakan For");
+console.log("Looping Pertama atau Maju");
+for (c = 1; c <= 5; c++) {
+    console.log(c);
+}
+
+console.log("Looping Kedua atau Mundur");
+for (d = 5; d >= 1; d--) {
+    console.log(d);
+}
+console.log();
 
 // 3. Angka Ganjil dan Genap
 // Hint: kamu akan menggunakan kondisional juga di kasus ini.
@@ -35,6 +52,17 @@ while (b >= 1) {
 // Apabila angka counter adalah angka genap, tuliskan GENAP
 // Apabila angka counter adalah angka ganjil, tuliskan GANJIL
 
+console.log("Soal Nomor 3. Angka Ganjil dan Genap");
+for (e = 1; e <= 100; e++) {
+    if (e % 2 == 0) {
+        console.log(e + " adalah bilangan genap");
+    }
+    else {
+        console.log(e + " adalah bilangan ganjil");
+    }
+}
+console.log();
+
 //4. counter kelipatan
 // Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
 // Pada 3 perulangan baru ini periksa setiap angka counter:
@@ -43,13 +71,39 @@ while (b >= 1) {
 // Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5, dan kelipatan 10 dengan pertambahan 9, tuliskan:
 // "3 kelipatan 3"dan seterusnya.
 
+console.log("Soal Nomor 4. counter kelipatan");
+for (f = 1; f <= 100; f++) {
+    if (f % 2 == 0) {
+        console.log(f + " adalah kelipatan 2");
+    }
+    else if (f % 5 == 0) {
+        console.log(f + " adalah kelipatan 5");
+    }
+    else if (f % 9 == 0) {
+        console.log(f + " adalah kelipatan 9");
+    }
+    else {
+        continue;
+    }
+}
+console.log();
+
 //5. Bintang asteriks
 //Problem buatlah bintang seperti berikut
-
-let input = 5;
 //hasilnya
 //*
 //**
 //***
 //****
 //*****
+
+console.log("Soal Nomor 5. Bintang asteriks");
+let result = "";
+for (x = 1; x <= 5; x++) {
+    for (y = 1; y <= x; y++) {
+        result += "*"
+    }
+    result += '\n';
+}
+console.log(result);
+
